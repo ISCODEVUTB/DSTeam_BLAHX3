2ff4727436678bcec46afde494414c7fd24afc5b
ordossss <iordosgoitia@utb.edu.co>
2025-03-05 00:04:19 -0500

    Update location.py

--- Gestion_Paquete/location.py
+++ Gestion_Paquete/location.py
@@ -1,13 +1,13 @@
 class Location(object):
     def __init__(self, country: str, department: str, city: str,
-                 address1: str, address2: str, zip_code: int):
+                 address1: str, address2: str, zip_code: int):  
         self.__country = country
         self.__department = department
         self.__city = city
         self.__address1 = address1
         self.__address2 = address2
-        self.__zip_code = zip_code 
-    
+        self.__zip_code = zip_code  
+
     @property
     def country(self) -> str:
         return self.__country
@@ -48,22 +48,23 @@ class Location(object):
     def address2(self, value: str):
         self.__address2 = value
     
+    #
     @property
-    def zip__code(self) -> int:
+    def zip_code(self) -> int:
         return self.__zip_code
     
     @zip_code.setter  
-    def zip__code(self, value: int):
+    def zip_code(self, value: int):
         self.__zip_code = value
 
-    
     def __str__(self):
-        return f"Country: {self.__country}, Department: {self.__department}, City: {self.__city}, Address 1: {self.__address1}, Address 2: {self.__address2}, Zip Code: {self.__zip__code}"
-    
+        return (f"Country: {self.__country}, Department: {self.__department}, City: {self.__city}, "
+                f"Address 1: {self.__address1}, Address 2: {self.__address2}, Zip Code: {self.__zip_code}")
 
+# PRUEBA DE LA CLASE
 def main():
     alex_location = Location("Colombia", "Bolivar", "Cartagena", "CRA", "Blas", 130001)
-    print (alex_location)
+    print(alex_location)
 
 if __name__ == '__main__':
     main()
