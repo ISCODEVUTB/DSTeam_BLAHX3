643e35a897ea8b165efa7134a38d1919a5f3c171
AnaSMH5 <anasmh05@hotmail.com>
2025-03-04 22:13:49 -0500

    Update test_location.py

--- Gestion_Paquete/test_Location.py
+++ Gestion_Paquete/test_Location.py
@@ -1,1 +1,18 @@
+import unittest
+from location import Location
+
+class TestLocation(unittest.TestCase):
+    def test_init(self):
+        """Ejecuta el constructor de la clase Location"""
+        self.location = Location(
+            country = "Colombia",
+            department = "Bolivar",
+            city = "Cartagena de Indias",
+            address1 = "Cra. 11 #39-21, San Diego",
+            address2 = "La Serrezuela",
+            zipCode = 130001
+        )
+
+if __name__ == "__main__":
+    unittest.main()
 
