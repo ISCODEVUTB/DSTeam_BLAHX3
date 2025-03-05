40c5fbd31d85e89098b6a7f043fd1eefcc6f2dfc
ordossss <iordosgoitia@utb.edu.co>
2025-03-05 15:18:23 -0500

    Update invoice.py

--- Gestion_Paquete/invoice.py
+++ Gestion_Paquete/invoice.py
@@ -1,35 +1,38 @@
 from datetime import date
 
+
 class Invoice:
-    def __init__(self, invoice_id: int, amount: float, date: date, status: str):
-        self.__invoice_id = invoice_id
-        self.__amount = amount
-        self.__date = date
-        self.__status = status
+    def __init__(self, invoice_id: int, amount: float, invoice_date: date, status: str):
+        self._invoice_id = invoice_id
+        self._amount = amount
+        self._invoice_date = invoice_date
+        self._status = status
 
     @property
     def invoice_id(self) -> int:
-        return self.__invoice_id
+        return self._invoice_id
 
     @property
     def amount(self) -> float:
-        return self.__amount
+        return self._amount
 
     @property
-    def date(self) -> date:
-        return self.__date
+    def invoice_date(self) -> date:
+        return self._invoice_date
 
     @property
     def status(self) -> str:
-        return self.__status
+        return self._status
 
     def __str__(self):
-        return f"Invoice(ID: {self.invoice_id}, Amount: ${self.amount:.2f}, Date: {self.date}, Status: {self.status})"
+        return f"Invoice(ID: {self.invoice_id}, Amount: ${self.amount:.2f}, Date: {self.invoice_date}, Status: {self.status})"
 
-#Prueba de la clase
+
+# Prueba de la clase
 def main():
     try_invoice = Invoice(14873, 254, date(2023, 4, 5), "Entregado")
     print(try_invoice)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
