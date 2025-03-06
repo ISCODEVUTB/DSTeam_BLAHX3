6775a00dfde6fc4f6ca5ce42e6ee6552fe5714f9
AnaSMH5 <anasmh05@hotmail.com>
2025-03-05 22:26:06 -0500

    Update delivery.py

--- Gestion_Paquete/delivery.py
+++ Gestion_Paquete/delivery.py
@@ -2,11 +2,11 @@ from track import Track
 
 
 class Delivery:
-    def __init__(self, order: int, status: str, track: Track, driver: str, position: str):
+    def __init__(self, order: int, status: str, track: Track, driver_id: int, position: str):
         self.__order = order
         self.__status = status
         self.__track = track
-        self.__driver = driver
+        self.__driver_id = driver_id
         self.__position = position
 
     @property
@@ -22,8 +22,8 @@ class Delivery:
         return self.__track
 
     @property
-    def driver(self) -> str:
-        return self.__driver
+    def driver(self) -> int:
+        return self.__driver_id
 
     @property
     def position(self) -> str:
@@ -34,5 +34,5 @@ class Delivery:
 
     def __str__(self):
         return (f"Delivery(Order: {self.order}, Status: {self.status}, "
-                f"Track: {self.track.track_id}, Driver: {self.driver}, "
+                f"Track: {self.track.track_id}, Driver: {self.driver_id}, "
                 f"Position: {self.position})")
