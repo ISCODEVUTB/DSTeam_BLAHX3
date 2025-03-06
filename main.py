f4d5e43c7833d5e2c714ae2891e4eecd95dcb43d
Hawasly7 <jesusjulio7622@gmail.com>
2025-03-06 10:30:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # import random
 from flask import Flask
-from flask_wtf import CSRFProtect
 from Gestion_Paquete.users import User
 from Gestion_Paquete.location import Location
 from Gestion_Paquete.package import Package
@@ -8,8 +7,6 @@ from Gestion_Paquete.package import Package
 in_user: User = [None]
 recipient_list: list = []
 app = Flask(__name__)
-csrf = CSRFProtect()
-csrf.init_app(app) 
 @app.route("/")
 
 def main():
