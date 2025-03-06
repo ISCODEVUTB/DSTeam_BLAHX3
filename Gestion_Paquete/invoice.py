d1e47f58a59b2243bd995044a0a10252b24d2f28
AnaSMH5 <anasmh05@hotmail.com>
2025-03-05 23:15:24 -0500

    Update invoice.py

--- Gestion_Paquete/invoice.py
+++ Gestion_Paquete/invoice.py
@@ -12,17 +12,33 @@ class Invoice:
     def invoice_id(self) -> int:
         return self._invoice_id
 
+    @invoice_id.setter
+    def invoice_id(self, value: int):
+        self._invoice_id = value
+
     @property
     def amount(self) -> float:
         return self._amount
+
+    @amount.setter
+    def amount(self, value: float):
+        self._amount = value
 
     @property
     def invoice_date(self) -> date:
         return self._invoice_date
 
+    @invoice_date.setter
+    def invoice_date(self, value: date):
+        self._invoice_date = value
+
     @property
     def status(self) -> str:
         return self._status
+
+    @status.setter
+    def status(self, value: str):
+        self._status = value
 
     def __str__(self):
         return (f"Invoice(ID: {self.invoice_id}, Amount: ${self.amount:.2f}, "
