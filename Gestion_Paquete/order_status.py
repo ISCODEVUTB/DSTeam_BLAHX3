2fa0ada0aaf48f409e5277372983d84e6aa74ae0
AnaSMH5 <anasmh05@hotmail.com>
2025-03-06 08:52:21 -0500

    Update order_status.py

--- Gestion_Paquete/order_status.py
+++ Gestion_Paquete/order_status.py
@@ -5,5 +5,7 @@ class OrderStatus(Enum):
     """
     Represents the possible statuses of an order.
     """
-    PENDING = 'pending'
-    PAYMENT_DUE_DATE = 'payment due date'
+    PENDING = "Pending"
+    SHIPPED = "Shipped"
+    DELIVERED = "Delivered"
+    CANCELED = "Canceled"
