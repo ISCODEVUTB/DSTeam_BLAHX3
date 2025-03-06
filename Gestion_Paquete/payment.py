74804bea92ed9cfec9ed75a76eb6f7eaa6138926
AnaSMH5 <anasmh05@hotmail.com>
2025-03-05 23:38:51 -0500

    Update payment.py

--- Gestion_Paquete/payment.py
+++ Gestion_Paquete/payment.py
@@ -9,8 +9,13 @@ class PaymentMethod(Enum):
     TRANSFER = 'Transfer'
     CASH = 'Cash'
 
+
 class Payment:
     def __init__(self, payment_id: int, amount: float, method: PaymentMethod, date: date):
+        # Check that the method initialized is part of payment method
+        if not isinstance(method, PaymentMethod):
+            raise ValueError("Invalid payment method")
+
         self.__payment_id = payment_id
         self.__amount = amount
         self.__method = method
@@ -49,4 +54,5 @@ class Payment:
         self.__date = value
 
     def __str__(self):
-        return f"Payment(ID: {self.payment_id}, Amount: ${self.amount:.2f}, Method: {self.method.value}, Date: {self.date.strftime('%Y-%m-%d')})"
+        return (f"Payment(ID: {self.payment_id}, Amount: ${self.amount:.2f}, "
+                f"Method: {self.method.value}, Date: {self.date.strftime('%Y-%m-%d')})")
