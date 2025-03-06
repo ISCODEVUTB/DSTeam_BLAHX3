3e04056dc965726bf660bad0b79893c8a54de032
ordossss <iordosgoitia@utb.edu.co>
2025-03-05 20:20:08 -0500

    Update test_package.py

--- test/test_package.py
+++ test/test_package.py
@@ -7,7 +7,7 @@ class TestPackage(unittest.TestCase):
         self.package = Package(
             package_id = 123456,
             weight = 10,
-            dimensions = "30*20*10",
+            dimensions = "30x20x10",
             package_type = "dimensionado"
         )
 
@@ -25,7 +25,7 @@ class TestPackage(unittest.TestCase):
         )
         self.assertEqual(
             self.package.dimensions,
-            "30*20*10",
+            "30x20x10",
             "Las dimensiones del paquete no se inicializó correctamente"
         )
         self.assertEqual(
@@ -38,14 +38,42 @@ class TestPackage(unittest.TestCase):
         """Asegura que la función calculate_price esté funcionado correctamente"""
 
         message = "El cálculo del precio es incorrecto."
-        # Test # 1: Paquete con peso de 10kg y dimensiones "30*20*10".
-        expected_price = 10 + 2 * 10 + len("30*20*10") * 0.5
+        # Test # 1: Paquete con peso de 10kg y dimensiones "30x20x10".
+        expected_price = 10 + 2 * 10 + len("30x20x10") * 0.5
         self.assertEqual (self.package.calculate_price(), expected_price, message)
 
         # Test #2: Paquete con peso de 5kg y dimensiones 2*5*5
-        package2 = Package(123457, 5.2, "2*5*5", "basico")
-        expected_price = 10 + 2 * 5.2 + len("2*5*5") * 0.5
+        package2 = Package(123457, 5.2, "2x5x5", "basico")
+        expected_price = 10 + 2 * 5.2 + len("2x5x5") * 0.5
         self.assertEqual (package2.calculate_price(), expected_price, message)
+
+    def test_invalid_dimensions_format(self):
+        """Verifica que se lance una excepción ValueError si las dimensiones no cuentan con el formato"""
+        with self.assertRaises(ValueError, msg = "El paquete debe poseer tres dimensiones"):
+            self.package.dimensions = "30x20"
+
+        with self.assertRaises(ValueError, msg = "Las dimensiones se deben dividir por un 'x'"):
+            self.package.dimensions = "2*5*5"
+
+    def test_validate_dimensions_non_numeric(self):
+        """Verifica que se lance una excepción ValueError si las dimensiones contiene valores no numéricos"""
+        with self.assertRaises(ValueError, msg = "Los únicos caracteres no numéricos que poseen las dimensiones son 'x'"):
+            self.package.dimensions = "30x20a10"
+
+        with self.assertRaises(ValueError, msg = "Las dimensiones no contienen valores no numéricos, excepto 'x'"):
+            self.package.dimensions = "30x2ax10"
+
+    def test_invalid_dimensions_empty(self):
+        """Verifica que se lance una excepción ValueError si las dimensiones están vacías"""
+        with self.assertRaises(ValueError):
+            self.package.dimensions = ""
+
+    def test_valid_dimensions(self):
+        """Verifica que no se lance una excepción para dimensiones correctas"""
+        try:
+            Package(123461, 10, "50x50x50", "dimensionado")  # Formato válido
+        except ValueError:
+            self.fail("Se lanzó ValueError para dimensiones válidas")
 
 
 if __name__ == "__main__":
