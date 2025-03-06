25ae2865d622c32ce8ff69c2a91182bb0cdc7336
AnaSMH5 <anasmh05@hotmail.com>
2025-03-06 08:38:09 -0500

    Update test_order.py to be useful for new order.py

--- test/test_order.py
+++ test/test_order.py
@@ -8,18 +8,21 @@ from Gestion_Paquete.location import Location
 class TestOrder(unittest.TestCase):
     def setUp(self):
         """This is executed before each test. Initializes data for Order which will be used for the tests later"""
-        # Setup the mock User
-        alex_location = Location("Colombia", "Bolivar", "Cartagena", "CRA", "Danis", 130001)
-        self.user = User("Alex", "Prens", "1944682", "a@gmail.com", alex_location, "Gatito123*")
+        # Setup the mock Users (sender and receiver)
+        sender_location = Location("Colombia", "Bolivar", "Cartagena", "CRA", "address", 130001)
+        receiver_location = Location("Colombia", "Atlantico", "Barranquilla", "CRA prueba", "address prueba", 80007)
+        self.sender = User("Alex", "Prens", "1944682", "a@gmail.com", sender_location, "Gatito123*")
+        self.sender = User("John", "Doe", "9102478", "john_doe@gmail.com", receiver_location, "Test*321")
 
-        # Setup some mock Packages
-        self.package1 = Package(10001, 25, "30x20x10", "basic")
-        self.package2 = Package(10002, 10, "50x50x50", "dimensioned")
+        # Setup mock Packages
+        self.package1 = Package(15, 1.0, 2.0, 3.0)
+        self.package2 = Package(5, 2.0, 3.0, 4.0)
 
         # Setup the Order with packages and user
         self.order = Order(
-            order_id=10001,
-            user=self.user,
+            order_id="10001",
+            sender=self.sender,
+            receiver=self.receiver,
             packages=[self.package1, self.package2],
             status="Pending"
         )
@@ -33,7 +36,7 @@ class TestOrder(unittest.TestCase):
 
     def test_add_package(self):
         """Verify that a new package can be added to the order"""
-        new_package = Package(10003, 3.5, "5x2x7", "standard")
+        new_package = Package(20, 10.0, 6.0, 1.5)
         self.order.add_package(new_package)
 
         # Assert that the new package is added
@@ -42,17 +45,17 @@ class TestOrder(unittest.TestCase):
 
     def test_remove_package(self):
         """Verify that a package can be removed from the order"""
-        self.order.remove_package(10001)
+        self.order.remove_package("10001")
 
         # Assert that the package with ID 101 is no longer in the list
-        removed_package = next((pkg for pkg in self.order.packages if pkg.package_id == 10001), None)
+        removed_package = next((pkg for pkg in self.order.packages if pkg.package_id == "10001"), None)
         self.assertIsNone(removed_package, "The package was not removed correctly.")
         self.assertEqual(len(self.order.packages), 1, "The number of packages did not update correctly after removal.")
 
     def test_invalid_remove_package(self):
         """Verify that attempting to remove a non-existing package ID does not alter the order"""
         initial_package_count = len(self.order.packages)
-        self.order.remove_package(19999)  # No package with ID 19999 exists
+        self.order.remove_package("19999")  # No package with ID 19999 exists
 
         # Assert that the package count remains the same
         self.assertEqual(len(self.order.packages), initial_package_count, "The package list was incorrectly altered.")
