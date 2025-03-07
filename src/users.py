d8fa44fe15934842c1d2ab11f268752c213bfa8c
AnaSMH5 <anasmh05@hotmail.com>
2025-03-06 22:35:16 -0500

    Update users.py

--- src/users.py
+++ src/users.py
@@ -15,7 +15,7 @@ Classes:
     - User: Eepresents a user with an ID, name, last name, national id, 
     email, address, password, and role.
 """
-import uuid
+import uuid1
 from bcrypt import hashpw, gensalt
 from src.location import Location
 from src.user_role import UserRole
